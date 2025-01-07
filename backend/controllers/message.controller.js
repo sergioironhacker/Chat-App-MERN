f83e3fc30a89de829625ef67aba0ea94ce9b55fc
Sergio Esteban <Sergioss772022@gmail.com>
2025-01-07 15:32:55 +0100

    feat: initial commit back and front

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,67 @@
+import Conversation from '../models/conversation.model.js';
+import Message from '../models/message.model.js';
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        if (!message) {
+            return res.status(400).json({ error: "Message content is required" });
+        }
+
+        // Busca la conversación entre los participantes
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+        });
+
+        // Si no existe la conversación, la crea
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+                messages: [], // Asegura que el arreglo de mensajes exista
+            });
+        }
+
+        // Crea y guarda el mensaje
+        const newMessage = new Message({
+            senderId: senderId,
+            receiverId: receiverId,
+            message: message,
+        });
+
+
+        if (newMessage) {
+            conversation.messages.push(newMessage._id);
+        }
+
+        /* await conversation.save();
+        await newMessage.save(); */
+
+        await Promise.all([conversation.save(), newMessage.save()]);
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+export const getMessages = async (req, res) => {
+    try {
+
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages);
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import connecToMongoDB from "./bd/connecToMongoDB.js";
+
+const app = express(); 
+const PORT = process.env.PORT || 5000; 
+
+
+dotenv.config();
+
+app.use(express.json()); // rep.body 
+app.use(cookieParser());
+
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+
+
+app.get("/", (req, res) => {
+
+    res.send("hello world")
+});
+
+
+
+
+app.listen(PORT, () => {
+    connecToMongoDB();
+    console.log(`Server running on port ${PORT}`)
+}); 
+
